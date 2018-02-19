6fb78c9857d55695dcd06a5545491396dadb4e88
yumere <yumere7833@gmail.com>
2018-02-19 17:43:12 +0900

    Add image_pool module to iterate data

---
+++ util/image_pool.py
@@ -0,0 +1,36 @@
+import random
+import torch
+from torch.autograd import Variable
+
+
+class ImagePool(object):
+    def __init__(self, pool_size):
+        self.pool_size = pool_size
+
+        if self.pool_size > 0:
+            self.num_images = 0
+            self.images = []
+
+    def query(self, images):
+        if self.pool_size == 0:
+            return Variable(images)
+
+        return_images = []
+        for image in images:
+            image = torch.unsqueeze(image, 0)  # ex) 3x10x10 image to 1x3x10x10
+            if self.num_images < self.pool_size:
+                self.num_images += 1
+                self.images.append(image)
+                return_images.append(image)
+            else:
+                p = random.uniform(0, 1)
+                if p > 0.5:
+                    random_id = random.randint(0, self.pool_size - 1)
+                    tmp = self.images[random_id].clone()
+                    self.images[random_id] = image
+                    return_images.append(tmp)
+                else:
+                    return_images.append(image)
+
+        return_images = Variable(torch.cat(return_images, 0))
+        return return_images
